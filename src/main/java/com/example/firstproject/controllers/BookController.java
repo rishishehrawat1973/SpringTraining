b5dff8e54c71b9ad2c678a35a50d786c4739281e
Rishi Shehrawat <rishishehrawat@gmail.com>
2021-05-04 14:05:54 +0530

    adding thymeleaf

---
+++ src/main/java/com/example/firstproject/controllers/BookController.java
@@ -0,0 +1,23 @@
+package com.example.firstproject.controllers;
+
+import com.example.firstproject.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books",bookRepository.findAll());
+
+        return "books/list";
+    }
+}
